6b53ede26d2128276b0a4b73936c3cb4eb348472
Hanaf Othmane <ohanaf@users.noreply.github.com>
2021-01-18 14:48:50 -0500

    add-functions-to-math.js added doublePi and tripplePi functions and import then to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi from "./math.js";
+import PI, { doublePi, triplePi } from "./math.js";
 
 ReactDOM.render(
 	<ul>
-		<li>{pi}</li>
-		<li>2</li>
-		<li>3</li>
+		<li>{PI}</li>
+		<li>{doublePi()}</li>
+		<li>{triplePi()}</li>
 	</ul>,
 	document.getElementById("root")
 );
